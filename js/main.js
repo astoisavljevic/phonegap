2336137d74e3e48474ca3a113289dbf11e12751f
Aleksandar Stoisavljevic <staleks@gmail.com>
2013-10-06 23:19:13 +0200

    [update:] working on about author page

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(function(){
 	
 	var imp = impress();
 	imp.init();
-	
+
 	$('#arrowLeft').click(function(e){
 		imp.prev();
 		e.preventDefault();
@@ -12,5 +12,23 @@ $(function(){
 		imp.next();
 		e.preventDefault();
 	});
+
+    $(document).on('impress:stepenter', function(e){
+        var currentSlide = $(e.target).attr('id');
+
+        if (currentSlide === 'aboutAuthor1') {
+            $("#universityDays").fadeIn(2000, function() {
+                $("#win32Days").fadeIn(3000);
+            });
+        } else if (currentSlide === 'aboutAuthor2') {
+            $("#levi9Days").fadeIn(2000)
+        } else if (currentSlide === 'aboutAuthor3') {
+            $("#hobby").fadeIn(2000, function() {
+                $("#contactDetails").fadeIn(1000, function() {
+                    $("#contactDetailsSpeaker").fadeIn(100);
+                });
+            });
+        };
+    });
 
 });
